0dcfc08796b8747b692ea34950a5be401b66a50d
ViniciusFarias <fsvinicius9@gmail.com>
2022-03-21 14:39:44 -0300

    criando a template na view pra ficar mais facil de se visualizar

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,21 +6,26 @@ class NegociacaoController {
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor"); // melhora a perfomance evitar percorrer o DOM inumeras vezes
     this._listaNegociacoes = new ListaNegociacoes();
+    this._negociacoesView = new NegociacoesView($("#negociacoesView"));
+    this._negociacoesView.update(this._listaNegociacoes);
   }
 
   adiciona(event) {
     event.preventDefault();
-    let negociacao = new Negociacao(
-      DateHelper.textoParaData(this._inputData.value),
-      this._inputQuantidade.value,
-      this._inputValor.value
-    )
 
-    this._listaNegociacoes.adiciona(negociacao)
+    this._listaNegociacoes.adiciona(this._criaNegociacao())
+    this._negociacoesView.update(this._listaNegociacoes)
     this._limpaFormulario();
 
     console.log(this._listaNegociacoes.negociacoes);
 
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      this._inputQuantidade.value,
+      this._inputValor.value)
   }
 
   _limpaFormulario() {
@@ -28,6 +33,6 @@ class NegociacaoController {
     this._inputQuantidade.value = ""
     this._inputValor.value = ""
 
-    this._inputQuantidade.focus();
+    this._inputData.focus();
   }
 }
