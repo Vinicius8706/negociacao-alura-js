908b2fafbe36cfeb4c1eca3aad6a6a0882c766b7
ViniciusFarias <fsvinicius9@gmail.com>
2022-03-21 10:13:35 -0300

    first commit

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,22 @@
+class Negociacao {
+  constructor(data, quantidade, valor) {
+    this._data = data;
+    this._quantidade = quantidade
+    this._valor = valor;
+  }
+
+  get volume() {
+    return this._quantidade * this.valor;
+  }
+
+  get quantidade(){
+    return this._quantidade;
+  }
+  get data(){
+    return this._data
+  }
+
+  get valor(){
+    return this._valor;
+  }
+}
